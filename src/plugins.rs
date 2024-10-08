fe35295530b6b5e6fa540a61a2c4a4ac312b705e
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2024-10-08 20:38:39 +0900

    add translation camera, but error in rotation

--- src/plugins.rs
+++ src/plugins.rs
@@ -13,23 +13,76 @@ impl Plugin for MoguraPlugins {
 }
 
 fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(15.0, 5.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    let target = Vec3::ZERO;
+    let up = Vec3::Y;
+    let init_pos = [0., 30., 0.];
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(init_pos[0], init_pos[1], init_pos[2])
+                .looking_at(target, up),
+            ..default()
+        },
+        CameraParams {
+            target,
+            ..Default::default()
+        },
+    ));
 }
 
 fn update_camera(
     mut mouse_motion: EventReader<MouseMotion>,
     mouse_button: Res<ButtonInput<MouseButton>>,
-    mut camera: Query<&mut Transform, With<Camera>>,
+    mut camera: Query<(&mut Transform, &mut CameraParams), With<Camera>>,
 ) {
     if mouse_button.pressed(MouseButton::Left) {
-        let mut camera = camera.single_mut();
+        let (mut transform, mut camera_params) = camera.single_mut();
+        dbg!(&transform);
         for motion in mouse_motion.read() {
-            dbg!(&motion.delta.x, &motion.delta.y);
-            dbg!(&camera);
+            match camera_params.mode {
+                CameraMode::Translation => {
+                    let (u_x, u_y, u_z) = get_unit_vectors_from_quat(transform.rotation);
+                    let delta_x = u_y * motion.delta.x * camera_params.sensitivity;
+                    let delta_y = u_z * motion.delta.y * camera_params.sensitivity;
+                    transform.translation += delta_x;
+                    transform.translation += delta_y;
+                    camera_params.target += delta_x;
+                    camera_params.target += delta_y;
+                }
+                CameraMode::Rotation => {
+                    let yaw = motion.delta.y * camera_params.sensitivity;
+                    let pitch = motion.delta.x * camera_params.sensitivity;
+                    let roll = 0.;
+                    let rotation_quat = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+                    transform.rotation = rotation_quat * transform.rotation;
+                    let radius = (transform.translation - camera_params.target).length();
+                    let (dir, _, _) = get_unit_vectors_from_quat(transform.rotation);
+                    transform.translation = camera_params.target - dir * radius;
+                }
+            }
         }
+    }
+
+    fn get_unit_vectors_from_quat(quat: Quat) -> (Vec3, Vec3, Vec3) {
+        let x = quat.x;
+        let y = quat.y;
+        let z = quat.z;
+        let w = quat.w;
+        let u_x = Vec3::new(
+            1.0 - 2.0 * (y * y + z * z),
+            2.0 * (x * y - w * z),
+            2.0 * (x * z + w * y),
+        );
+        let u_y = Vec3::new(
+            2.0 * (x * y + w * z),
+            1.0 - 2.0 * (x * x + z * z),
+            2.0 * (y * z - w * x),
+        );
+        let u_z = Vec3::new(
+            2.0 * (x * z - w * y),
+            2.0 * (y * z + w * x),
+            1.0 - 2.0 * (x * x + y * y),
+        );
+        (u_x.normalize(), u_y.normalize(), u_z.normalize())
     }
 }
 
@@ -90,3 +143,28 @@ struct Ground;
 
 #[derive(Component)]
 struct SimpleSphere;
+
+#[derive(Component, Debug)]
+struct CameraParams {
+    pub target: Vec3,
+    pub mode: CameraMode,
+    pub sensitivity: f32,
+}
+
+impl Default for CameraParams {
+    fn default() -> Self {
+        Self {
+            target: Vec3::ZERO,
+            // mode: CameraMode::Translation,
+            mode: CameraMode::Rotation,
+            sensitivity: 0.01,
+            // sensitivity: 0.1,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum CameraMode {
+    Rotation,
+    Translation,
+}
