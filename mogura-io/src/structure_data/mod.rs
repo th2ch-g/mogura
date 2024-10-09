cb050c4b3ade4f8a58a984b5a6822cf06ec05b44
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2024-10-09 11:54:08 +0900

    rearrange to workspace

---
+++ mogura-io/src/structure_data/mod.rs
@@ -0,0 +1,64 @@
+pub mod pdb;
+use crate::structure_data::pdb::PDBData;
+
+pub fn load(structure_file: &str) -> impl StructureData {
+    let extension = std::path::Path::new(structure_file)
+        .extension()
+        .and_then(|ext| ext.to_str());
+    if let Some(extension) = extension {
+        match extension {
+            "pdb" => PDBData::load(structure_file),
+            "gro" => {
+                todo!();
+            }
+            _ => {
+                unimplemented!("This extension is not supported.")
+            }
+        }
+    } else {
+        panic!("structure_file: {} has no extension.", structure_file);
+    }
+}
+
+pub trait StructureData {
+    fn load(structure_file: &str) -> Self
+    where
+        Self: Sized;
+    // fn export(output_path: &str);
+    fn atoms(&self) -> &Vec<Atom>;
+}
+
+#[derive(Debug, Clone)]
+pub struct Atom {
+    id: usize,
+    model_id: usize,
+    chain_name: String,
+    residue_id: isize,
+    residue_name: String,
+    atom_id: usize,
+    atom_name: String,
+    element: Option<pdbtbx::Element>,
+    x: f32, // angstrom
+    y: f32, // angstrom
+    z: f32, // angstrom
+            // charge: usize,
+}
+
+impl Atom {
+    pub fn x(&self) -> f32 {
+        self.x
+    }
+    pub fn y(&self) -> f32 {
+        self.y
+    }
+    pub fn z(&self) -> f32 {
+        self.z
+    }
+    pub fn xyz(&self) -> [f32; 3] {
+        [self.x, self.y, self.z]
+    }
+}
+
+// pub struct Element {
+//
+// }
---
+++ mogura-io/src/structure_data/pdb.rs
@@ -0,0 +1,57 @@
+use crate::structure_data::*;
+
+pub struct PDBData {
+    atoms: Vec<Atom>,
+}
+
+impl StructureData for PDBData {
+    fn load(structure_file: &str) -> Self
+    where
+        Self: Sized,
+    {
+        let content = std::fs::read_to_string(structure_file).unwrap();
+        let reader = std::io::BufReader::new(std::io::Cursor::new(content));
+        let (input_pdb, _errors) = pdbtbx::open_pdb_raw(
+            reader,
+            pdbtbx::Context::show("a.pdb"), // random anme
+            pdbtbx::StrictnessLevel::Loose,
+        )
+        .unwrap();
+        let mut id = 0;
+        let mut atoms = Vec::new();
+        for i in 0..input_pdb.model_count() {
+            let model = input_pdb.model(i).unwrap();
+            for j in 0..input_pdb.chain_count() {
+                let chain = input_pdb.chain(j).unwrap();
+                for k in 0..chain.residue_count() {
+                    let residue = chain.residue(k).unwrap();
+                    for l in 0..residue.atom_count() {
+                        let atom = residue.atom(l).unwrap();
+                        atoms.push(Atom {
+                            id,
+                            model_id: model.serial_number(),
+                            chain_name: chain.id().to_string(),
+                            residue_id: residue.serial_number(),
+                            residue_name: match residue.name() {
+                                Some(name) => name.to_string(),
+                                None => "None".to_string(),
+                            },
+                            atom_id: atom.serial_number(),
+                            atom_name: atom.name().to_string(),
+                            element: atom.element().copied(),
+                            x: atom.x() as f32,
+                            y: atom.y() as f32,
+                            z: atom.z() as f32,
+                        });
+                        id += 1;
+                    }
+                }
+            }
+        }
+        Self { atoms }
+    }
+
+    fn atoms(&self) -> &Vec<Atom> {
+        &self.atoms
+    }
+}
