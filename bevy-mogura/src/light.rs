f3a4fd84a77ce77941bd30a69f02576983cac370
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2024-10-14 14:02:05 +0900

    set light

--- bevy-mogura/src/light.rs
+++ bevy-mogura/src/light.rs
@@ -2,7 +2,19 @@ use bevy::prelude::*;
 
 pub fn setup_light(mut commands: Commands) {
     commands.spawn(DirectionalLightBundle {
-        transform: Transform::from_translation(Vec3::ONE).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-0.1, 0.1, 1.).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+    commands.spawn(DirectionalLightBundle {
+        transform: Transform::from_xyz(1., 2., 0.5).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+    commands.spawn(DirectionalLightBundle {
+        transform: Transform::from_xyz(-1., 2., -1.).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+    commands.spawn(DirectionalLightBundle {
+        transform: Transform::from_xyz(-1., -1., 0.).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 }
