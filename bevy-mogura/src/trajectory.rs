1b0a856ec6d76ce809e95d31b0b59a750935509e
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-02-18 19:03:26 +0900

    fmt

--- bevy-mogura/src/trajectory.rs
+++ bevy-mogura/src/trajectory.rs
@@ -8,7 +8,10 @@ pub fn update_trajectory(
     mut current_visualized_atoms: Query<(&mut Transform, &AtomID), Without<BondID>>,
     mut current_visualized_bonds: Query<(&mut Transform, &BondID), Without<AtomID>>,
 ) {
-    if mogura_state.update_trajectory || mogura_state.update_tmp_trajectory || mogura_state.loop_trajectory {
+    if mogura_state.update_trajectory
+        || mogura_state.update_tmp_trajectory
+        || mogura_state.loop_trajectory
+    {
         if mogura_state.trajectory_data.is_some() {
             let current_frame_id = mogura_state.current_frame_id;
             let frame = mogura_state
